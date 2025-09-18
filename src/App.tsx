3976eacb6682fe4966cacf28599d08d03fc1b50a
Asit D <33679711+LazyBuilder@users.noreply.github.com>
2025-09-18 17:56:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 // Import the new component at the top of the file
 import VerveComponent from './verve_website.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
